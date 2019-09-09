680b81fb1d15aa73122a61baae4e30e5cde87785
rokeby <gary.zhexi.zhang@gmail.com>
2019-09-09 18:23:35 +0100

    new website for nablus gas stations

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+var gas = new Array("assets/img/gas1.jpg")
+
+var gasNumber = 2; 
+var gasTotal = 24;
+
+while (gasNumber < gasTotal) {
+	gasNumber++;
+	console.log(gasNumber);
+	gas.push("assets/img/gas" + gasNumber + ".jpg")
+}
+
+var max = gas.length;
+
+var currentImage = $("img").attr("src");
+
+var currentID = currentImage.replace(/.*\.*\.jpg/, '$1');
+
+var currentIndex = 0
+
+while (currentIndex < max) {
+	if (gas[currentIndex] == currentID) {
+		break;
+	}
+	currentIndex++;
+}
+
+	$(function() {
+		$("img").click(function(){
+			currentIndex = (currentIndex + 1) % max;
+			$("img").attr('src', gas[currentIndex]);
+		});
+	});
+});
